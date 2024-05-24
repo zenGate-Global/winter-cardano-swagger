2f5ab1fe911086233a320c87144b97dd46567a2a
mgpai <64056966+mgpai22@users.noreply.github.com>
2024-05-24 17:18:11 -0400

    initial

--- src/App.js
+++ src/App.js
@@ -1,25 +1,5 @@
-import logo from './logo.svg';
-import './App.css';
+import SwaggerUI from "swagger-ui-react";
+import "swagger-ui-react/swagger-ui.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
-
-export default App;
+let App; // eslint-disable-line no-unused-vars
+export default App = () => <SwaggerUI url="/openapi.json" />;
